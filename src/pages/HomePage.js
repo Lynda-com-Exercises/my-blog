a23af710ab5076ed6400419a2b1ee7e156a010b8
Eduardo Valerio Gamez <evaleriog@outlook.com>
2020-04-03 15:18:23 -0500

    project setup and first commit

---
+++ src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HomePage = () => (
+    <React.Fragment>
+        <h1>Hello, welcome to my blog</h1>
+        <p>kndelknxklndklendklnldneklndlkende</p>
+        <p>kndelknxklndklendklnldneklndlkende</p>
+        <p>kndelknxklndklendklnldneklndlkende</p>
+        <p>kndelknxklndklendklnldneklndlkende</p>
+    </React.Fragment>
+
+)
+
+export default HomePage;
